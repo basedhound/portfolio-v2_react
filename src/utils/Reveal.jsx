55500da7698d27cf67f2dcd5e42fe35dd50cfedb
Jozef S <franck.vukelic@gmail.com>
2024-07-27 19:26:14 +0700

    Update viewport once to true

--- src/utils/Reveal.jsx
+++ src/utils/Reveal.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 export const Reveal = ({ children }) => {
   return (
     <motion.div
-      whileInView={{ opacity: [0, 1] }}
       viewport={{ once: true }}
+      whileInView={{ opacity: [0, 1] }}
       transition={{ duration: 1.5, delay: 0.25 }}>
       {children}
     </motion.div>
@@ -14,8 +14,8 @@ export const Reveal = ({ children }) => {
 export const SlideReveal = ({ children }) => {
   return (
     <motion.div
-      whileInView={{ opacity: [0, 1], y: [100, 0] }}
       viewport={{ once: true }}
+      whileInView={{ opacity: [0, 1], y: [100, 0] }}
       transition={{ duration: 0.5, delay: 0.25 }}>
       {children}
     </motion.div>
