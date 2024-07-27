913638a965240192eefac194d070cd523630b6d8
Jozef S <franck.vukelic@gmail.com>
2024-07-27 18:33:01 +0700

    Update index

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -5,6 +5,7 @@ import work1 from "./work1.jpg";
 import work2 from "./work2.jpg";
 import work3 from "./work3.jpg";
 import work4 from "./work4.jpg";
+import work5 from "./work5.jpg";
 
 import testimonial1 from "./testimonial1.png";
 import testimonial2 from "./testimonial2.png";
@@ -18,6 +19,7 @@ export default {
   work2,
   work3,
   work4,
+  work5,
   testimonial1,
   testimonial2,
   testimonial3,
